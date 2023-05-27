7a831c4b07a7fd14ea7ee7065658a3a058fd9714
RiaVouKnovsial <volovik.raisa.kam@gmail.com>
2023-05-27 19:56:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1. Реализуйте класс "StringSet", представляющий множество строк. 
+//Перегрузите операторы объединения (+) и пересечения(&) для множеств строк. 
+//Создайте два объекта типа "StringSet" и выполните операции объединения и пересечения между ними.
+
+using System;
+
+Console.WriteLine("Массив строк: ");
+
+StringSet s1 = new StringSet { Value = "January, February, March, April, May, June, " };
+StringSet s2 = new StringSet { Value = "July, August, September, October, November, December" };
+
+Console.WriteLine(s1.Value);
+Console.WriteLine(s2.Value);
+Console.WriteLine();
+
+Console.WriteLine("Конкатенация строк: ");
+StringSet s3 = s1 + s2;
+Console.WriteLine(s3.Value);
+Console.WriteLine();
+
+Console.WriteLine("Пересечение строк: ");
+StringSet s4 = s1 & s2;
+Console.WriteLine(s3.Value);
+
+
---
+++ StringSet.cs
@@ -0,0 +1,19 @@
+﻿class StringSet
+{
+    public string Value { get; set; }
+
+    public static StringSet operator +(StringSet s1, StringSet s2)
+    {
+        return new StringSet { Value = s1.Value + s2.Value };
+    }
+
+    public static StringSet operator &(StringSet s1, StringSet s2)
+    {
+        string[] s1Values = s1.Value.Split(", ");
+        string[] s2Values = s2.Value.Split(", ");
+
+        string intersection = string.Join(", ", s1Values.Intersect(s2Values));
+
+        return new StringSet { Value = intersection };
+    }
+}
